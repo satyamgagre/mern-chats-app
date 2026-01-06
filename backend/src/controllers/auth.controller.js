e847c34b70ff931e478898e828af5ce891b6e971
Satyam Gagare <satyamgagre4@gmail.com>
2026-01-07 00:59:10 +0530

    Update auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -53,7 +53,7 @@ export const signup = async (req, res) => {
   }
 };
 
-
+//login
 export const login = async (req, res) => {
   const {email, password} = req.body
   try {
@@ -83,6 +83,13 @@ export const login = async (req, res) => {
   }
 };
 
+//logout
 export const logout = (req, res) => {
-    res.send("logout route.");
+  try {
+    res.cookie("jwt", "", {maxAge:0})
+    res.status(200).json({message: "Logged out successfully."});
+  } catch (error) {
+      console.log("Error in logout controller.", error.message);
+      req.status(500).json({message: "Internal Server Error"})
+  }
 };
