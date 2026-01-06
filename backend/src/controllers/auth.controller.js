84631f6b28c983fdaee969fa8371cebd6fcc14fb
Satyam Gagare <satyamgagre4@gmail.com>
2026-01-06 10:31:31 +0530

    06-01-2026

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 
+//signup
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
 
