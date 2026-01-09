5b1e8349ece7c3bd7423a04bd1fb84573ab057b1
Satyam Gagare <satyamgagre4@gmail.com>
2026-01-10 02:36:25 +0530

    implement sendMessage logic in message.controllers

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -32,3 +32,36 @@ export const getMessages = async(req, res) => {
         res.status(500).json({ error: "Internal Server Error."});
     }
 }
+
+export const sendMessage = async (req, res) => {
+    try {
+        const { text, image } = req.body;
+        const { id: receiverId } = req.params;
+        const senderId = req.user._id;
+        
+        let imageUrl;
+        if(image){
+            //Upload base64 image to directory
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url; 
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl,
+        });
+        
+        await newMessage.save();
+
+
+        // todo: realtime functionality goes here => socket.io 
+
+        res.status(201).json(newMessage);
+
+    } catch (error) {
+        console.log("Error in sendMessage controller: ", error.message);
+        res.status(500).json({message: "Internal Server Error."});
+    }
+}
