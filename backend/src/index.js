a5983cf026392fef7b15d2742ff13944ab5ee4eb
Satyam Gagare <satyamgagre4@gmail.com>
2026-01-11 02:44:34 +0530

    frontend setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ import authRoutes  from "./routes/auth.route.js";
 import messageRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 
@@ -13,8 +14,11 @@ const PORT = process.env.PORT || 5001;
 
 // Middleware to parse JSON requests
 app.use(express.json());
-
 app.use(cookieParser());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}))
 
 // Routes
 app.use("/api/auth", authRoutes);
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className="bg-primary text-white p-4 text-xl">
+      Navbar
+    </div>
+  );
+};
+
+export default Navbar;
